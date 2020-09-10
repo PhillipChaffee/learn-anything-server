20dc7a0f51459c339fce4fe089cb5b5b41361769
Phillip Chaffee <phillipdensmorechaffee@gmail.com>
2020-09-10 07:29:49 -0700

    Category & Resource API
    - Finished fixing the last bug

--- src/controller/CategoryController.ts
+++ src/controller/CategoryController.ts
@@ -15,13 +15,19 @@ export class CategoryController {
     }
 
     async save(request: Request, response: Response, next: NextFunction) {
-        return this.categoryRepository.save(request.body);
+        let newCategory = request.body;
+        if (!newCategory.name) {
+            response.status(400);
+            response.send("New categories must have a name.")
+        }
+
+        return this.categoryRepository.save(newCategory);
     }
 
     async remove(request: Request, response: Response, next: NextFunction) {
         let categoryToRemove = await this.categoryRepository.findOne(request.params.id);
 
-        if(categoryToRemove.resources && categoryToRemove.resources.length > 0){
+        if (categoryToRemove.resources && categoryToRemove.resources.length > 0) {
             response.status(409);
             response.send("Cannot delete a category that still has resources.");
         }
--- src/controller/ResourceController.ts
+++ src/controller/ResourceController.ts
@@ -1,19 +1,16 @@
 import {getRepository} from "typeorm";
 import {NextFunction, Request, Response} from "express";
 import {Resource} from "../entity/Resource";
+import {Category} from "../entity/Category";
 
 export class ResourceController {
 
     private resourceRepository = getRepository(Resource);
+    private categoryRepository = getRepository(Category);
 
     async all(request: Request, response: Response, next: NextFunction) {
         if (request.query.categoryId) {
-            return this.resourceRepository.
-                createQueryBuilder("resource").
-                innerJoin("resource.category", "category").
-                where("category.id = :categoryId").
-                setParameter("categoryId", request.query.categoryId).
-                getMany();
+            return this.resourceRepository.createQueryBuilder("resource").innerJoin("resource.category", "category").where("category.id = :categoryId").setParameter("categoryId", request.query.categoryId).getMany();
         }
 
         return this.resourceRepository.find();
@@ -24,6 +21,14 @@ export class ResourceController {
     }
 
     async save(request: Request, response: Response, next: NextFunction) {
+        let category = await this.categoryRepository.findOneOrFail(request.body.categoryId);
+        if (!category.resources) {
+            category.resources = [];
+        }
+
+        category.resources.push(request.body);
+        await this.categoryRepository.save(category);
+
         return this.resourceRepository.save(request.body);
     }
 
