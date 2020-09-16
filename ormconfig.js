f23a14a0f3174ea694dc970dacec7d6616c86836
Phillip Chaffee <phillipdensmorechaffee@gmail.com>
2020-09-16 10:23:29 -0700

    Fix entity discovery issue.

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-    "type": process.env.TYPEORM_CONNECTION,
-    "host": process.env.TYPEORM_HOST,
-    "port": process.env.TYPEORM_PORT,
-    "username": process.env.TYPEORM_USERNAME,
-    "password": process.env.TYPEORM_PASSWORD,
-    "database": process.env.TYPEORM_DATABASE,
+    "type": process.env.LA_CONNECTION,
+    "host": process.env.LA_HOST,
+    "port": process.env.LA_PORT,
+    "username": process.env.LA_USERNAME,
+    "password": process.env.LA_PASSWORD,
+    "database": process.env.LA_DATABASE,
     "synchronize": true,
     "logging": false,
     "entities": [
